cd46f2b5026b29b95758ab17c9e462118d60cacc
niguangting@gmail.com <nig@flip2.engr.oregonstate.edu>
2015-06-07 15:10:33 -0700

    Done

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def main(input_file):
 	start_time = timeit.default_timer()
 
 	#get the total length and path 
-	#len_path = tsp_greedy(cities, coords)
 	len_path = tsp_dp(cities, coords)
 	end_time = timeit.default_timer()
 
@@ -60,13 +59,7 @@ def get_distance(coords, i, j):
 	distance = round(math.sqrt(pow((iCoordx - jCoordx),2)+pow((iCoordy - jCoordy),2)))
 	return distance
 
-#Get the total distance of a path
-def get_length(coords, path, cities):
-	dis = 0
-	for i in range(len(path)):
-		dis = dis + get_distance(coords, cities[path[i]], cities[path[i - 1]])
-	return dis
-
+#Get the minimum distance from city endpoint to all the cities in unvisited list
 def nearest(coords, unvisited, endpoint):
 	nearest_dis = float('inf')
 	for i in unvisited.keys():
@@ -76,59 +69,64 @@ def nearest(coords, unvisited, endpoint):
 			nearest_dis = cur_dis
 	return (nearest_city, nearest_dis)
 		
-
+#Dynamic programming: Add the nearest city(from unvisited cities to endpoints)
+#to the path, until all the cities are added to the path.
 def tsp_dp(cities, coords):
-        #distances = dict()
 	sub_length = get_distance(coords, 0, 1)
 	end_point_1 = 0;
+
+	#Define a list to hold the path of shortest path
 	path = list()
+
+	#Define a dictionary to hold visited cities
 	visited = dict()
+	#Define a dictionary to hold unvisited cities
 	unvisited = dict()
 	for i in range(0, len(cities)):
 		unvisited[i] = True
 
+	#start the path with city 0
 	path.append(0)
 	visited[0] = True
 	del unvisited[0]
+
+	#Get the nearest city to city 0
 	(nearest_city, sub_length) = nearest(coords, unvisited, 0)
 	path.append(nearest_city)
 	visited[nearest_city] = True
 	del unvisited[nearest_city]
+
+	#Initailize the two endpoints
 	ep_front = 0
 	ep_end = nearest_city
 
 
+	#In the loop, add the nearest city to endpoints to path
 	while(len(unvisited) > 0):
+		#Get the nearest city to the front endpoint
+		#and the distance between those two cities
 		(nearest_front, front_dis) = nearest(coords, unvisited, ep_front)
+		
+		#Get the nearest city to the end endpoint
+		#and the distance between those two cities
 		(nearest_end, end_dis) = nearest(coords, unvisited, ep_end)
-		#print("nearest_front " + str(nearest_front))
-		#print("nearest_end " + str(nearest_end))
 
-
-		#print("front_dis " + str(front_dis))
-		#print("end_dis " + str(end_dis))
-
+		#Compare the above two minimum distances
 		if(front_dis < end_dis):
+			#reset front endpoint
 			ep_front = nearest_front
 			path.insert(0, nearest_front) 
 			sub_length = front_dis + sub_length
-			#print("ep_front " + str(ep_front))
 			del unvisited[ep_front]
 		else:
+			#reset end endpoint
 			ep_end = nearest_end
-			#print("ep_end " + str(ep_end))
 			path.append(nearest_end)
 			sub_length = end_dis + sub_length
 			del unvisited[ep_end]
-		#print("path ")
-		#print (path)
-		#print("unvisited")
-		#print(unvisited)
-		#print("sub_length:" + str(sub_length))
 
+	#Get the length of the tour/path
 	length = int(round(sub_length + get_distance(coords, ep_front, ep_end)))
-	print(path)
-	#print(cities)
 	print(length)
 	return [length] + path
 
