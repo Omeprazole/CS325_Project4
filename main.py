7f223118f10ff69e0e1a74414d58677afeb0af7c
niguangting@gmail.com <nig@flip3.engr.oregonstate.edu>
2015-06-07 11:31:59 -0700

    Need add comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main(input_file):
 	cities = list()
 
 	#Define a list to hold the coordinates of each city
-	coords = dict()
+	coords = list()
 	
 	#Define a list to hold the lenght(The first element) and path
 	len_path = list()
@@ -23,11 +23,9 @@ def main(input_file):
 	#Extract data from file
 	for city in cities_raw:
 		temp = city.split()
-		xCoord = str(temp[0]) + 'x'
-		yCoord = str(temp[0]) + 'y'
-		coords[xCoord] = int(temp[1])
-		coords[yCoord] = int(temp[2])
-		cities.append(temp[0])
+		coord = (int(temp[1]), int(temp[2]))
+		coords.append(coord)
+		cities.append(int(temp[0]))
 	ifile.close()
 	
 	#record time
@@ -48,17 +46,17 @@ def main(input_file):
 
 	#Output results into output file
 	for i in range(len(len_path) ):
-		ofile.write(len_path[i] + "\n")
+		ofile.write(str(len_path[i]) + "\n")
 
 
 	ofile.close()
 
 #Calculate distance from city i to city j
 def get_distance(coords, i, j):
-	iCoordx = coords[str(i)+'x']
-	iCoordy = coords[str(i)+'y']
-	jCoordx = coords[str(j)+'x']
-	jCoordy = coords[str(j)+'y']
+	iCoordx = coords[i][0]
+	iCoordy = coords[i][1]
+	jCoordx = coords[j][0]
+	jCoordy = coords[j][1]
 	distance = round(math.sqrt(pow((iCoordx - jCoordx),2)+pow((iCoordy - jCoordy),2)))
 	return distance
 
@@ -71,12 +69,12 @@ def get_length(coords, path, cities):
 
 def nearest(coords, unvisited, endpoint):
 	nearest_dis = float('inf')
-	for i in range(len(unvisited)):
-		cur_dis = get_distance(coords, unvisited[i], endpoint)
+	for i in unvisited.keys():
+		cur_dis = get_distance(coords, i, endpoint)
 		if(cur_dis < nearest_dis):
-			nearest_city = unvisited[i]
+			nearest_city = i
 			nearest_dis = cur_dis
-	return nearest_city
+	return (nearest_city, nearest_dis)
 		
 
 def tsp_dp(cities, coords):
@@ -84,146 +82,55 @@ def tsp_dp(cities, coords):
 	sub_length = get_distance(coords, 0, 1)
 	end_point_1 = 0;
 	path = list()
+	visited = dict()
 	unvisited = dict()
 	for i in range(0, len(cities)):
-		unvisited.append(i)
+		unvisited[i] = True
 
 	path.append(0)
-	unvisited.remove(0)
-	nearest_city = nearest(coords, unvisited, 0)
+	visited[0] = True
+	del unvisited[0]
+	(nearest_city, sub_length) = nearest(coords, unvisited, 0)
 	path.append(nearest_city)
-	unvisited.remove(nearest_city)
+	visited[nearest_city] = True
+	del unvisited[nearest_city]
 	ep_front = 0
 	ep_end = nearest_city
-	print("first nearest city" + str(nearest_city))
-	print("fisrt end" + str(nearest_city))
-	sub_length = get_distance(coords, 0, nearest_city) 
 
 
-
-	#front_flag = 1
-	#end_flag = 1
 	while(len(unvisited) > 0):
-		front_flag = 1
-		end_flag = 1
-		for i in range(len(cities)):
-			visited_flag = 0
-			for j in range(len(path)):
-				if(i == path[j]):
-					visited_flag = 1
-					break
-	
-			if(visited_flag == 1):
-				continue
-			print("front_flag " + str(front_flag))		
-			print("end_flag " + str(end_flag))		
-
-			if(front_flag == 1): 
-				print("front near")
-				nearest_front = nearest(coords, unvisited, ep_front)
-				front_dis = get_distance(coords, nearest_front, ep_front)
-			if(end_flag == 1):
-				print("end near")
-				nearest_end = nearest(coords, unvisited, ep_end)
-				end_dis = get_distance(coords, nearest_end, ep_end)
-			#print("nearest_front " + str(nearest_front))
-			#print("nearest_end " + str(nearest_end))
+		(nearest_front, front_dis) = nearest(coords, unvisited, ep_front)
+		(nearest_end, end_dis) = nearest(coords, unvisited, ep_end)
+		#print("nearest_front " + str(nearest_front))
+		#print("nearest_end " + str(nearest_end))
 
 
-			print("front_dis " + str(front_dis))
-			print("end_dis " + str(end_dis))
+		#print("front_dis " + str(front_dis))
+		#print("end_dis " + str(end_dis))
 
-			if(front_dis < end_dis):
-				ep_front = nearest_front
-				path.insert(0, nearest_front) 
-				sub_length = front_dis + sub_length
-				print("ep_front " + str(ep_front))
-				unvisited.remove(ep_front)
-				#end_flag = 0
-				front_flag = 1
-			else:
-				ep_end = nearest_end
-				print("ep_end " + str(ep_end))
-				path.append(nearest_end)
-				sub_length = end_dis + sub_length
-				unvisited.remove(ep_end)
-				#front_flag = 0
-				end_flag = 1
-			print("path ")
-			print (path)
-			#print("unvisited")
-			#print(unvisited)
-			print("sub_length:" + str(sub_length))
+		if(front_dis < end_dis):
+			ep_front = nearest_front
+			path.insert(0, nearest_front) 
+			sub_length = front_dis + sub_length
+			#print("ep_front " + str(ep_front))
+			del unvisited[ep_front]
+		else:
+			ep_end = nearest_end
+			#print("ep_end " + str(ep_end))
+			path.append(nearest_end)
+			sub_length = end_dis + sub_length
+			del unvisited[ep_end]
+		#print("path ")
+		#print (path)
+		#print("unvisited")
+		#print(unvisited)
+		#print("sub_length:" + str(sub_length))
 
-	length = sub_length + get_distance(coords, ep_front, ep_end)
-	#length_1 = get_length(coords, path, cities)
+	length = int(round(sub_length + get_distance(coords, ep_front, ep_end)))
 	print(path)
 	#print(cities)
-	print("path_lenght " + str(len(path)))
 	print(length)
-	#print(length_1)
 	return [length] + path
-
-
-#Greedy algorithm for tsp problem
-"""
-def tsp_greedy(cities, coords):
-	unvisited = dict()
-	current_path = list()
-	current_length = 0
-	current_city = -1
-	closest_city = -1
-	shortest_path = list()
-	shortest_length = float('inf')
-
-	for i in range(0, len(cities)):
-		unvisited[i] = i
-	current_path = list()
-	shortest_length = float('inf')
-	current_length = 0
-
-	distances = dict()
-	for i in range(0, len(cities)):
-		for j in range (0, len(cities)):
-			distances[str(i)+ '-' + str(j)] = get_distance(coords, i, j)
-
-	for i in range(0, len(cities)):
-		for i in range(0, len(cities)):
-			unvisited[i] = i
-		current_path = list()
-		shortest_length = float('inf')
-		current_length = 0
-
-		current_city = cities[i]
-		del unvisited[i]
-
-		current_path.append(str(i))
-		while(len(unvisited) > 0):
-			closest_distance = float('inf')
-			closest_city = -1
-			for each in unvisited:
-				distance = distances[str(each)+ '-' + str(current_city)]
-				if distance > 0 and distance < closest_distance:
-					closest_distance = distance
-					closest_city = each
-			current_city = closest_city
-			current_path.append(str(closest_city))
- 			del unvisited[closest_city]
-			current_length = current_length + closest_distance
-		if current_length < shortest_length:
- 			shortest_length = current_length
-			shortest_path = current_path
-
-	#Add the distance from last city to the first city to shortest length
-	start = shortest_path[0]
-	end = shortest_path[len(shortest_path) - 1]
-	shortest_length = shortest_length + distances[str(start)+'-'+str(end)]
-
-	#Convert float to integer
-	shortest_length = int(round(shortest_length))
-	return [str(shortest_length)] + shortest_path
-"""
-
 
 if __name__ == "__main__":
 	sys.exit(main(sys.argv[1]))
